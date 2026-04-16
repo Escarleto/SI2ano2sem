81b86374dab162de6b6c230661bdda9fb9d97973
Escarleto <lucascliberato31@gmail.com>
2026-04-16 10:18:51 +0100

    Squashed commit of the following:
    commit 6dd2f52ab0306be4e9c4ea21ec28a9861af5df55
    Author: JokinhasProgramassoes <joaotomaspereiraribeiro2004@gmail.com>
    Date:   Thu Apr 16 10:06:41 2026 +0100
    
        UI Problems
    
        O UI foi resolvido mas nao funciona com o  comando.... :(
    
    commit 58ed87f62b0ce164f81dfea4967293949805d0fa
    Author: JokinhasProgramassoes <joaotomaspereiraribeiro2004@gmail.com>
    Date:   Wed Apr 15 19:02:57 2026 +0100
    
        Squashed commit of the following:
    
        commit c9e274d6e334d19d008e20f3fb88d5e2b25997c6
        Author: Escarleto <lucascliberato31@gmail.com>
        Date:   Wed Apr 15 16:27:17 2026 +0100
    
            Player pronto, ainda mais items adicionados
    
        commit 7777968a8209b8465e63077043d0ef3b793239e4
        Author: Escarleto <lucascliberato31@gmail.com>
        Date:   Tue Apr 14 18:48:10 2026 +0100
    
            Player ainda mais polido, acrescentamento inicial de UI
    
        commit b06094489b014d174f050f29bb3ae1d97a5a4ecf
        Author: Escarleto <lucascliberato31@gmail.com>
        Date:   Mon Apr 13 18:39:01 2026 +0100
    
            Player melhorado + almondega adicionada
    
    commit e14a7e273f1931cd80262c0f033e0be131d455d0
    Author: JokinhasProgramassoes <joaotomaspereiraribeiro2004@gmail.com>
    Date:   Tue Apr 14 19:28:44 2026 +0100
    
        Fixed Patches
    
        recuperado :P
    
    commit 8168d361b73e4c607d454846a1f9225d08acd933
    Author: JokinhasProgramassoes <joaotomaspereiraribeiro2004@gmail.com>
    Date:   Tue Apr 14 19:17:24 2026 +0100
    
        Squashed commit of the following:
    
        commit 7777968a8209b8465e63077043d0ef3b793239e4
        Author: Escarleto <lucascliberato31@gmail.com>
        Date:   Tue Apr 14 18:48:10 2026 +0100
    
            Player ainda mais polido, acrescentamento inicial de UI
    
        commit b06094489b014d174f050f29bb3ae1d97a5a4ecf
        Author: Escarleto <lucascliberato31@gmail.com>
        Date:   Mon Apr 13 18:39:01 2026 +0100
    
            Player melhorado + almondega adicionada
    
    commit 89a7150efafeb594e3859f1ad99ac59eb4150c39
    Author: JokinhasProgramassoes <joaotomaspereiraribeiro2004@gmail.com>
    Date:   Tue Apr 14 19:15:33 2026 +0100
    
        TEste
    
        BLEH :P
    
    commit b08b67a0c1cf3a45f7155cecac8c648dc51cf7da
    Author: JokinhasProgramassoes <joaotomaspereiraribeiro2004@gmail.com>
    Date:   Tue Apr 14 14:58:38 2026 +0100
    
        Costumize Scene
    
        Criei uma cena para fazer a costumização
    
        Falta: Linkar o codigo do lucas às setas, fazer com que grave a escolha do player ......
    
        JokasProgramassoes 👅

--- Assets/Scripts/HatManager.cs
+++ Assets/Scripts/HatManager.cs
@@ -1,15 +1,73 @@
+//using System;
 using UnityEngine;
 
 public class HatManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] Hats; // Array para armazenar os chapéus correspondentes a cada jogador, com um limite de 4 jogadores
-    [SerializeField] private GameObject HatAnchor; // Referência ao objeto vazio que representa o local onde o chapéu deve ser posicionado
+    [SerializeField] private GameObject HatPlace; // Referência ao objeto vazio que representa o local onde o chapéu deve ser posicionado
     private PlayerController Player; // Referência ao PlayerController para acessar o índice do jogador
+    GameObject currentHat;
+    public GameObject UI;
+    public bool Confirmed;
+
+
+    int currentHatIndex = 0;
 
     private void Start()
     {
         Player = GetComponent<PlayerController>(); // Obtém o PlayerController do jogador para acessar o índice do jogador
         if (Player == null) return;
-        Instantiate(Hats[Player.PlayerIndex], HatAnchor.transform); // Instancia o chapéu correspondente ao jogador usando o índice do jogador para acessar o array de chapéus
+
+
+        UpdateHat();
+        //Instantiate(Hats[Player.PlayerIndex], HatPlace.transform); // Instancia o chapéu correspondente ao jogador usando o índice do jogador para acessar o array de chapéus
+    }
+
+    void UpdateHat()
+    {
+        if (currentHat != null)
+        {
+            Destroy(currentHat);
+        }
+
+        currentHat = Instantiate(Hats[currentHatIndex], HatPlace.transform);
+
+        currentHat.transform.localPosition = Vector3.zero;
+        currentHat.transform.localRotation = Quaternion.identity;
+
+        if (Confirmed == true)
+        {
+            UI.SetActive(false);
+        }
+
+    }
+
+    public void NextHat(int ToHat)
+    {
+        currentHatIndex++;
+
+        if (currentHatIndex >= Hats.Length)
+            currentHatIndex = 0;
+
+        UpdateHat();
+        Debug.Log("Proximo Chapeu");
+    }
+
+    public void PreviousHat()
+    {
+        Debug.Log("Chapeu anterior");
+        currentHatIndex--;
+
+        if (currentHatIndex < 0)
+            currentHatIndex = Hats.Length - 1;
+
+        UpdateHat();
+    }
+
+    public void SaveHat()
+    {
+        Confirmed = true;
+        PlayerPrefs.SetInt("HatIndex", currentHatIndex);
+        PlayerPrefs.Save();
     }
 }
